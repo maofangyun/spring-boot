b4b551508fdbbb067db7958985f313e48acda446
maofangyun <maofangyun@gmail.com>
2020-11-03 18:59:38 +0800

    首次提交

---
+++ my-test/src/main/java/com/mfy/test/SpringBootRunTest.java
@@ -0,0 +1,11 @@
+package com.mfy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRunTest {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRunTest.class,args);
+	}
+}
---
+++ my-test/src/main/java/com/mfy/test/bean/TestBean.java
@@ -0,0 +1,7 @@
+package com.mfy.test.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean {
+}
